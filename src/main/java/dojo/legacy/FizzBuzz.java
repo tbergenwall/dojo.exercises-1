74857d08b8b6f3740ac670e6e5010363de65814c
Thomas Bergenwall <thomas.bergenwall@ericsson.com>
2012-06-05 11:58:33 +0300

    Refactored code and test cases

--- src/main/java/dojo/legacy/FizzBuzz.java
+++ src/main/java/dojo/legacy/FizzBuzz.java
@@ -1,25 +1,28 @@
 package dojo.legacy;
 
 public class FizzBuzz {
-	 public String[] play(int start, int stop) {
-		 if (start > 0 && start <= stop) {
-		     int length = stop - start + 1;
-		     String[] array = new String[length];
-		     for (int i = 0; i < length; i++) {
-		         if ((start + i) % 15 == 0) {
-		                 array[i]="Fizzbuzz";
-		         } else
-		                 if (start + i % 5 == 0) {
-		                 array[i]="Buzz";
-		                 } else { if ((start + i) % 3 == 0)
-		                 array[i]="Fizz";
-		         else {
-		                 array[i] =  "" + (start + i);
-		         }}
-		     }
-		     return array;
-		 } else {
-		     throw new IllegalArgumentException();
-		 }
-	 }
+	public String[] play(int start, int stop) {
+		checkArgs(start, stop);
+		int length = stop - start + 1;
+		String[] array = new String[length];
+		for (int i = 0; i < length; i++) {
+			int test = start + i;
+			if (test % 15 == 0) {
+				array[i]="Fizzbuzz";
+			} else if (test % 5 == 0) {
+				array[i]="Buzz";
+			} else if (test % 3 == 0) {
+				array[i]="Fizz";
+			} else {
+				array[i] =  Integer.toString(test);
+			}
+		}
+		return array;
+	}
+
+	private void checkArgs(int start, int stop) {
+		if (start <= 0 || start > stop) {
+			throw new IllegalArgumentException();
+		}		
+	}
 }
